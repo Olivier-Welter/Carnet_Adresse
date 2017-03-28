6e5d20eda8ba3e651d1767c361ad0f5384c476ef
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-03-28 11:27:45 +0200

    Refactorage nom

--- src/carnetadresse/TestArrayList.java
+++ src/carnetadresse/TestArrayList.java
@@ -9,6 +9,10 @@ package carnetadresse;
  *
  * @author Olivier
  */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
  
@@ -20,8 +24,33 @@ public class TestArrayList {
   public static void main(String[] args) {
 
          
+      //*****************************
+      
+      
+       try {
+                // enregistrement
+                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream ("E:\\contact.dat"));
+                out.writeObject("Sauvegarde de l'ArrayList");
+                out.writeObject(new Contact("Gomez", "Michel","0652659865", "0523659874","toto@toto.com","tata@tata.com","58 rue des signe - 31 000 Toulouse"));
+                out.close();
+                // lecture
+                ObjectInputStream in = new ObjectInputStream(new FileInputStream ("E:\\contact.dat"));
+                String s = (String) in.readObject();
+                System.out.println(s);
+                Contact p = (Contact) in.readObject();
+                System.out.println(p);
+                in.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace ();
+            }
+      
+      //******************************
+      
+      
 
     ArrayList al = new ArrayList();
+    
 
     al.add(new Contact("Gomez", "Michel","0652659865", "0523659874","toto@toto.com","tata@tata.com","58 rue des signe - 31 000 Toulouse"));
 
