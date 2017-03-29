8030ee8631e73fc396f74657971d7e4a22d1d444
VincentTescari <vincent.tescari.pro@gmail.com>
2017-03-29 14:15:57 +0200

    Recherche v1.1

--- src/carnetAdresseV3/model/Recherche.java
+++ src/carnetAdresseV3/model/Recherche.java
@@ -1,88 +1,102 @@
 package carnetAdresseV3.model;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Olive, Vince & Kéké
  */
-
 public final class Recherche {
-    
+
     private final String recherche;
     private ArrayList listRecherche;
     private String mssgErreur = null;
-    
+
     /**
      * Constructeur recherche la chaîne passée en paramètre
+     *
      * @param recherche
      */
-    
-    public Recherche(String recherche, ArrayList listInfos)
-    {
+    public Recherche(String recherche, ArrayList listInfos) {
         this.recherche = recherche;
-        if (rechercheCorrecte()) rechercheContact(listInfos);
+        if (rechercheCorrecte()) {
+            rechercheContact(listInfos);
+        }
     }
-        
+
     /**
      * Retoune la liste résultant de la recherche
-     * @return 
+     *
+     * @return
      */
-    
-    public ArrayList getListRecherche()
-    {
+    public ArrayList getListRecherche() {
         return listRecherche;
     }
-    
+
     /**
      * Retoune la chaîne recherchée
-     * @return 
+     *
+     * @return
      */
-    
-    public String getRecherche()
-    {
+    public String getRecherche() {
         return recherche;
     }
-    
+
     /**
      * Retoune le message d'erreur s'il y en a un
-     * @return 
+     *
+     * @return
      */
-    
-    public String getMssgErreur()
-    {
+    public String getMssgErreur() {
         return mssgErreur;
     }
-    
+
     /**
      * Vérifie si la recherche n'est pas vide
-     * @return 
+     *
+     * @return
      */
-    
-    public Boolean rechercheCorrecte()
-    {
+    public Boolean rechercheCorrecte() {
         Boolean reponse = false;
-
-        if ((recherche != null && !recherche.isEmpty()))
-        {
-            reponse = true;
-        }
-        else mssgErreur = "Tu n'as rien recherché sale batard !";
         
+        if ((recherche != null && !recherche.isEmpty())) {
+            reponse = true;
+        } else {
+            mssgErreur = "Il faut renseigner le champ de recherche !";
+        }
+
         return reponse;
     }
-    
+
     /**
      * Recherche
      */
-    
-    public void rechercheContact(ArrayList listInfos)
-    {
+    public void rechercheContact(ArrayList listInfos) {
+        // Initialisation des données pour la recherche :
+        Pattern pattern;
+        Matcher matcher;
         // Fonction recherche contact
-        
-        
-        
         // Stocke la liste résultant de la recherche
-        listRecherche = listInfos;
+        listRecherche = new ArrayList();
+        int i;
+        // Boucle de parcours de notre liste 
+        for (i = 0; i < listInfos.size(); i++) {
+            ArrayList ligne = (ArrayList) listInfos.get(i);
+            // Affectation des valeurs pour la recherche sans sensibilité à la casse
+            pattern = Pattern.compile(recherche,Pattern.CASE_INSENSITIVE);
+            matcher = pattern.matcher((String)ligne.get(0));
+            // Comparaison 
+            if (matcher.find()) {
+                // Si correspondance, ajout dans la liste 
+                listRecherche.add(listInfos.get(i));
+            }
+        }
+        // Si pas de résultat : 
+        if (listRecherche.isEmpty())
+            mssgErreur = "Pas de résultat pour cette recherche !";
     }
-    
 }
