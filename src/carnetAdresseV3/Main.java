a3a22a74819805948185e6adbeeb50994fa3c5f3
VincentTescari <vincent.tescari.pro@gmail.com>
2017-03-29 17:24:48 +0200

    v4.3

--- src/carnetAdresseV3/Main.java
+++ src/carnetAdresseV3/Main.java
@@ -34,39 +34,44 @@ public class Main extends Application {
     private Stage currentStage;
     static private boolean showTable = false;
     private final ObservableList<TableItem> tableContent = FXCollections.observableArrayList();
-    
-    @FXML private Label labelErreur;
-    @FXML private TextArea contactAdresse;
-    @FXML private TableView<TableItem> table;
-    @FXML private TableColumn<TableItem, String> tableNom;
-    @FXML private TableColumn<TableItem, String> tablePrenom;
-    @FXML private TableColumn<TableItem, String> tablePhone;
-    @FXML private TableColumn<TableItem, String> tableAdresse;
-    @FXML private TableColumn<TableItem, String> tableMail;
-    @FXML private TextField contactRecherche,contactNom,contactPrenom,contactFixePhone,contactMobilePhone,contactMailPro,contactMailPerso;
-    
 
-    
+    @FXML
+    private Label labelErreur;
+    @FXML
+    private TextArea contactAdresse;
+    @FXML
+    private TableView<TableItem> table;
+    @FXML
+    private TableColumn<TableItem, String> tableNom;
+    @FXML
+    private TableColumn<TableItem, String> tablePrenom;
+    @FXML
+    private TableColumn<TableItem, String> tablePhone;
+    @FXML
+    private TableColumn<TableItem, String> tableAdresse;
+    @FXML
+    private TableColumn<TableItem, String> tableMail;
+    @FXML
+    private TextField contactRecherche, contactNom, contactPrenom, contactFixePhone, contactMobilePhone, contactMailPro, contactMailPerso;
+
     /**
      * Démarrage de l'interface graphique
+     *
      * @throws java.io.IOException
      */
     @Override
-    public void start(Stage primaryStage) throws IOException
-    {
+    public void start(Stage primaryStage) throws IOException {
         currentStage = primaryStage;
-        currentStage.setTitle("L'anale des libertins toulousains");
-       
+        currentStage.setTitle("L'annuaire des libertins toulousains");
         changePage("view/AccueilView.fxml");
-
     }
 
     /**
      * Affiche la page dont la vue est passée en paramètre
+     *
      * @fxml
      */
-    private void changePage(String fxml) throws IOException
-    {
+    private void changePage(String fxml) throws IOException {
         showTable = fxml.equals("view/AccueilView.fxml");
         // Récupère le contenu du fichier fxml
         Parent content = FXMLLoader.load(getClass().getResource(fxml));
@@ -74,20 +79,20 @@ public class Main extends Application {
         currentStage.setScene(new Scene(content));
         // Met à jour la fenêtre
         currentStage.show();
-        
+
     }
-    
+
     @FXML
-    protected void initialize() throws IOException
-    {
-        if(showTable) listeInfos(Contact.lectureFichierContact());
+    protected void initialize() throws IOException {
+        if (showTable) {
+            listeInfos(Contact.lectureFichierContact());
+        }
     }
-    
+
     /**
      * Liste les infos sur la page d'accueil
      */
-    private void listeInfos(ArrayList<ArrayList> listInfos) throws IOException
-    {
+    private void listeInfos(ArrayList<ArrayList> listInfos) throws IOException {
         tableContent.clear();
         tableNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
         tablePrenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
@@ -95,79 +100,83 @@ public class Main extends Application {
         tableAdresse.setCellValueFactory(new PropertyValueFactory<>("adresse"));
         tableMail.setCellValueFactory(new PropertyValueFactory<>("mail"));
 
-        for(ArrayList<String> ligne:listInfos)
-        {
-            tableContent.add(new TableItem(ligne.get(0),ligne.get(1),ligne.get(2),ligne.get(3),ligne.get(4)));
+        for (ArrayList<String> ligne : listInfos) {
+            tableContent.add(new TableItem(ligne.get(0), ligne.get(1), ligne.get(2), ligne.get(3), ligne.get(4)));
         }
         table.setItems(tableContent);
     }
-    
+
     /**
      * Bouton RECHERCHE
+     *
      * @param event
      */
     @FXML
-    private void actionBoutonRecherche(ActionEvent event) throws IOException
-    {
+    private void actionBoutonRecherche(ActionEvent event) throws IOException {
         // Instancie Recherche avec la chaîne contenue dans le champ de saisie de recherche
         Recherche currentRecherche = new Recherche(contactRecherche.getText(), Contact.lectureFichierContact());
-        if(currentRecherche.getMssgErreur() != null)
-        {
-            labelErreur.setText("");
-            listeInfos(Contact.lectureFichierContact());
-        }
-        else
-        {
+        if (currentRecherche.getMssgErreur() != null) {
+            // Affichage de l'erreur
+            labelErreur.setText(currentRecherche.getMssgErreur());
+            // Si recherche est vide, affichage de la liste complète
+            if (contactRecherche.getText().equals("")) {
+                listeInfos(Contact.lectureFichierContact());
+            } else {
+                // Sinon aucun affichage
+                tableContent.clear();
+            }
+        } else {
             labelErreur.setText("Recherche de " + currentRecherche.getRecherche());
             listeInfos(currentRecherche.getListRecherche());
         }
     }
-    
+
     /**
      * Bouton AJOUTER CONTACT
+     *
      * @param event
      */
     @FXML
-    private void actionBoutonContact(ActionEvent event) throws IOException
-    {
+    private void actionBoutonContact(ActionEvent event) throws IOException {
         // Récupère la fenêtre où le bouton a été cliqué puis change de page
         currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         changePage("view/ContactView.fxml");
     }
-    
+
     /**
      * Bouton ENREGISTRER
+     *
      * @param event
      */
     @FXML
-    private void actionBoutonEnregistrer(ActionEvent event) throws IOException
-    {
+    private void actionBoutonEnregistrer(ActionEvent event) throws IOException {
         // Instancie Contact avec la chaîne contenue dans le champ de saisie du nom
-        Contact currentContact = new Contact(contactNom.getText(),contactPrenom.getText(),contactFixePhone.getText(),contactMobilePhone.getText(),contactMailPro.getText(),contactMailPerso.getText(),contactAdresse.getText());
-        if(currentContact.getMssgErreur() != null) labelErreur.setText(currentContact.getMssgErreur());
-        else labelErreur.setText("Contact " + currentContact.getNom() + " enregistré !");
+        Contact currentContact = new Contact(contactNom.getText(), contactPrenom.getText(), contactFixePhone.getText(), contactMobilePhone.getText(), contactMailPro.getText(), contactMailPerso.getText(), contactAdresse.getText());
+        if (currentContact.getMssgErreur() != null) {
+            labelErreur.setText(currentContact.getMssgErreur());
+        } else {
+            labelErreur.setText("Contact " + currentContact.getNom() + " enregistré !");
+        }
     }
-    
+
     /**
      * Bouton RETOUR
+     *
      * @param event
      */
     @FXML
-    private void actionBoutonRetour(ActionEvent event) throws IOException
-    {
+    private void actionBoutonRetour(ActionEvent event) throws IOException {
         // Récupère la fenêtre où le bouton a été cliqué puis change de page
         currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         changePage("view/AccueilView.fxml");
     }
 
-    
-    
     /**
      * Le main lance la méthode start avec Application.launch()
+     *
      * @param args
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 }
